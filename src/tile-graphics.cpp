6e673905e86af77a88fcf90c0e59d36bc7a76a2e
GyuMin Lee <lovekm0121@naver.com>
2025-06-08 05:16:57 +0900

    타일 숫자 중앙정렬로 가독성 개선 #4

--- src/tile-graphics.cpp
+++ src/tile-graphics.cpp
@@ -9,6 +9,14 @@
 namespace Game {
 namespace {
 
+std::string centerString(const std::string& str, int width) {
+    int len = str.length();
+    if (width <= len) return str;
+    int pad_left = (width - len) / 2;
+    int pad_right = width - len - pad_left;
+    return std::string(pad_left, ' ') + str + std::string(pad_right, ' ');
+}  
+
 Color::Modifier tileBgColor(ull value) {
   switch (value) {
     case 2: return bg2;
@@ -38,16 +46,19 @@ Color::Modifier tileFgColor(ull value) {
 } // namespace
 
 std::string drawTileString(tile_t currentTile) {
-  std::ostringstream tile_richtext;
-  if (!currentTile.value) {
-    tile_richtext << "    ";
-  } else {
-    tile_richtext << tileBgColor(currentTile.value)
-                  << tileFgColor(currentTile.value)
-                  << bold_on << std::setw(4)
-                  << currentTile.value << bold_off << def;
-  }
-  return tile_richtext.str();
+    std::ostringstream tile_richtext;
+    const int tile_width = 4; // 타일 칸의 너비에 맞게 조정
+    if (!currentTile.value) {
+        tile_richtext << std::string(tile_width, ' ');
+    } else {
+        std::string number = std::to_string(currentTile.value);
+        tile_richtext << tileBgColor(currentTile.value)
+                      << tileFgColor(currentTile.value)
+                      << bold_on
+                      << centerString(number, tile_width)
+                      << bold_off << def;
+    }
+    return tile_richtext.str();
 }
 
 } // namespace Game
